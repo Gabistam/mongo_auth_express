84a2b878312525313a0761c2ae81207b898f787e
Gabistam <48634603+Gabistam@users.noreply.github.com>
2023-09-16 15:22:00 +0200

    config db & app.js

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const { initDatabase } = require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Connexion à la base de données
+initDatabase();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
