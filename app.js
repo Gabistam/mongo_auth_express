6cc68c616f6311a451def432d818cbd9b0cb84f4
Gabistam <48634603+Gabistam@users.noreply.github.com>
2023-09-25 01:53:18 +0200

    ready to play

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 var express = require('express');
 const { bizareDB } = require('./config/database');
-const PORT = process.env.PORT || 3000;
+
 
 var app = express();
 
 // Connexion à la base de données
 bizareDB();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 // Route de base pour tester
 app.get('/', (req, res) => {
   res.send('Yeah ! Ça marche ! Je suis dans le navigateur ! 🎉🚀');
 });
 
-app.listen(PORT, () => {
-	console.log(`🚀🚀 Lancement avec succès du port ${PORT}`);
+app.listen(3333, () => {
+	console.log(`🚀🚀 Lancement avec succès du server`);
 });
 
 module.exports = app;
