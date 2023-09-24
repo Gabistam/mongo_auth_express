47baef9e902c590b5763916447b833cd1e1156ab
Gabistam <48634603+Gabistam@users.noreply.github.com>
2023-09-25 00:26:36 +0200

    strict minimum

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var express = require('express');
 const { bizareDB } = require('./config/database');
+const PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Connexion à la base de données
 bizareDB();
-
-
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +13,10 @@ app.use(express.urlencoded({ extended: false }));
 // Route de base pour tester
 app.get('/', (req, res) => {
   res.send('Yeah ! Ça marche ! Je suis dans le navigateur ! 🎉🚀');
+});
+
+app.listen(PORT, () => {
+	console.log(`🚀🚀 Lancement avec succès du port ${PORT}`);
 });
 
 module.exports = app;
