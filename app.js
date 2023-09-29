b6d273fd6caa6e681777cd2e67a1e0ffd751cbf3
Gabistam <48634603+Gabistam@users.noreply.github.com>
2023-09-29 12:32:12 +0200

    maj

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const session = require('express-session');
@@ -46,7 +44,6 @@ app.set('view engine', 'twig');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Utilisez les routes définies dans routes.js
