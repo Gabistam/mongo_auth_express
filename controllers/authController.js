90fbd8eb7fd01d7a690a2eeed65d40c66297b26b
Gabistam <48634603+Gabistam@users.noreply.github.com>
2023-10-06 16:45:06 +0200

    install dependences

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,36 @@
 const passport = require('passport');
 
-exports.login = (req, res, next) => {
-    console.log("Tentative de connexion");  // Ajout d'un log
-    passport.authenticate('local', (err, user, info) => {
-        console.log("Dans la fonction d'authentification");  // Ajout d'un log
-        if (err) {
-            console.log("Erreur :", err);  // Ajout d'un log
-            req.session.flash = [{ type: 'danger', message: 'Une erreur est survenue.' }];
-            return next(err);
-        }
+exports.login = async (req, res, next) => {
+    try {
+        console.log("Tentative de connexion");  // Ajout d'un log
+        const user = await new Promise((resolve, reject) => {
+            passport.authenticate('local', (err, user, info) => {
+                if (err) reject(err);
+                resolve(user);
+            })(req, res, next);
+        });
+
         if (!user) {
             console.log("Utilisateur non trouvé ou mot de passe incorrect");  // Ajout d'un log
             req.session.flash = [{ type: 'warning', message: 'Identifiants incorrects.' }];
             return res.redirect('/login');
         }
-        req.logIn(user, (err) => {
-            if (err) {
-                console.log("Erreur lors de la connexion :", err);  // Ajout d'un log
-                req.session.flash = [{ type: 'danger', message: 'Une erreur est survenue.' }];
-                return next(err);
-            }
-            console.log("Connexion réussie");  // Ajout d'un log
-            req.session.flash = [{ type: 'success', message: 'Connexion réussie.' }];
-            return res.redirect('/profile');
+
+        await req.logIn(user, (err) => {
+            if (err) throw err;
         });
-    })(req, res, next);
+
+        console.log("Connexion réussie");  // Ajout d'un log
+        req.session.flash = [{ type: 'success', message: 'Connexion réussie.' }];
+        return res.redirect('/profile');
+
+    } catch (err) {
+        console.log("Erreur :", err);  // Ajout d'un log
+        req.session.flash = [{ type: 'danger', message: 'Une erreur est survenue.' }];
+        return next(err);
+    }
 };
+
 
 
 exports.showLoginPage = (req, res) => {
